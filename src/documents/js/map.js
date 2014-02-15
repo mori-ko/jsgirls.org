ab0b88a87181dc90ea54d57c86d749fef8d455cd
1000ch <orz1000ch@gmail.com>
2014-02-16 02:52:20 +0900

    fix

--- src/documents/js/map.js
+++ src/documents/js/map.js
@@ -1,38 +1,33 @@
-var mapCanvas = document.getElementById('js-map-canvas');
-
-if (mapCanvas) {
-  google.maps.event.addDomListener(window, 'load', function () {
-    var map = new google.maps.Map(mapCanvas, {
-      
-    });
-  });
-}
-
 jsgirls.Map = function (selector, options) {
   this.map = null;
-  var that = this;
-  google.maps.event.addDomListener(window, 'load', function () {
-    var mapCanvas = document.querySelector(selector);
-    if (!mapCanvas) {
-      throw new Error('Map canvas does not exist.');
-    }
-    var lat = options.lat || 35.6606782;
-    var lng = options.lng || 139.72862716;
-    var latlng = new google.maps.LatLng(lat, lng);
-    that.map = new google.maps.Map(mapCanvas, {
-      center: latlng,
-      zoom: options.zoom || 12,
-      mapTypeControl: true,
-      overviewMapControl: true,
-      panControl: true,
-      rotateControl: false,
-      scaleControl: true,
-      zoomControl: true
-    });
-    var marker = new google.maps.Marker({
-      position: latlng,
-      title: options.title || 'Google Japan'
-    });
-    marker.setMap(that.map);
+
+  // get map div
+  var mapCanvas = document.querySelector(selector);
+  if (!mapCanvas) {
+    throw new Error('Map canvas does not exist.');
+  }
+  
+  // get latitude and longitude
+  var lat = options.lat || 35.6606782;
+  var lng = options.lng || 139.72862716;
+  var latlng = new google.maps.LatLng(lat, lng);
+  
+  // initialize map
+  this.map = new google.maps.Map(mapCanvas, {
+    center: latlng,
+    zoom: options.zoom || 12,
+    mapTypeControl: true,
+    overviewMapControl: true,
+    panControl: true,
+    rotateControl: false,
+    scaleControl: true,
+    zoomControl: true
   });
+  
+  // put pin and set it
+  var marker = new google.maps.Marker({
+    position: latlng,
+    title: options.title || 'Google Japan'
+  });
+  marker.setMap(this.map);
 };
